e24f2b2063e5303c25ed26c21b50e45565b07da2
Callum Messiter <callum.messiter@gmail.com>
2025-07-20 15:16:09 +0100

    Compose plotting logic

--- cortexlib/cortexlib/utils/plotting.py
+++ cortexlib/cortexlib/utils/plotting.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+import pandas as pd
+
 gabor_filter_colours = {
     'gabor_small': '#cccccc',
     'gabor_medium': '#969696',
@@ -19,3 +23,74 @@ vgg19_colours = {
     'conv5_4': '#e34a33',
     'fc2': '#b30000',
 }
+
+
+def plot_mean_fev(avg_metrics, individual_metrics, remove_gabor=False):
+    """
+    Plot mean FEV with error bars.
+    Parameters:
+    - avg_metrics: DataFrame with metrics for each layer of each model, averaged across mice
+    - individual_metrics: DataFrame with individual mouse metrics for each layer of each model, with 
+    """
+    # Clean labels and sort
+    ordered_labels = [
+        'small', 'medium', 'large',
+        'layer1', 'layer2', 'layer3', 'layer4', 'fc',
+        'conv2_2', 'conv3_4', 'conv4_4', 'conv5_4', 'fc2'
+    ]
+    for df in [avg_metrics, individual_metrics]:
+        df["layer"] = df["layer"].str.replace(r"^gabor_", "", regex=True)
+        df["layer"] = pd.Categorical(
+            df["layer"], categories=ordered_labels, ordered=True)
+
+    avg_metrics = avg_metrics.sort_values('layer')
+
+    # Colour mapping by model
+    model_colour = {
+        'simclr': simclr_colours['layer4'],
+        'vgg19': vgg19_colours['conv5_4'],
+        'gabor': gabor_filter_colours['gabor_large']
+    }
+
+    avg_metrics["colour"] = avg_metrics["model_target"].str.lower().map(
+        lambda s: next((v for k, v in model_colour.items() if k in s), None)
+    )
+
+    if remove_gabor:
+        # Remove Gabor from plot
+        avg_metrics = avg_metrics[~avg_metrics["model_target"].str.contains(
+            "gabor", case=False)]
+        individual_metrics = individual_metrics[~individual_metrics["model_target"].str.contains(
+            "gabor", case=False)]
+
+    # Plot
+    plt.figure(figsize=(6, 5))
+
+    # Error bars
+    plt.bar(
+        avg_metrics['layer'],
+        avg_metrics['mean_fev'],
+        yerr=avg_metrics['sem_fev'],
+        capsize=5,
+        color=avg_metrics['colour']
+    )
+
+    # Individual dots
+    for _, row in individual_metrics.iterrows():
+        plt.scatter(
+            row["layer"], row["mean_fev"],
+            color=model_colour[row["model_target"].split("_")[0]],
+            edgecolor='black', s=30, alpha=0.8, zorder=3
+        )
+
+    # Axis and legend
+    plt.ylabel('Mean FEV')
+    plt.xlabel('Model layer')
+    plt.xticks(rotation=45, ha='right')
+    plt.legend(
+        handles=[Patch(facecolor=color, label=model.capitalize())
+                 for model, color in model_colour.items() if model != "gabor"],
+        title="Model"
+    )
+    plt.tight_layout()
+    plt.show()
